c0be0054dab679b9d53486cc23386e6db3b7934f
langmanleguang <langmanleguang@qq.com>
2017-12-01 10:14:02 +0800

    add demo.

---
+++ app/src/main/java/cn/itsite/aguider/demo/demo/ActivityActivity.java
@@ -0,0 +1,20 @@
+package cn.itsite.aguider.demo.demo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import cn.itsite.aguider.demo.R;
+/**
+ * @author leguang
+ * @version v0.0.0
+ * @E-mail langmanleguang@qq.com
+ * @time 2016/11/24 0024 9:08
+ */
+public class ActivityActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_activity);
+    }
+}
