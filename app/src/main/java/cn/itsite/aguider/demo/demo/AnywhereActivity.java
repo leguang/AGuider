54b0ddb551872bb1031264f73c45efb10943dd21
langmanleguang <langmanleguang@qq.com>
2017-12-01 10:37:58 +0800

    add demo.

---
+++ app/src/main/java/cn/itsite/aguider/demo/demo/AnywhereActivity.java
@@ -0,0 +1,15 @@
+package cn.itsite.aguider.demo.demo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import cn.itsite.aguider.demo.R;
+
+public class AnywhereActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_anywhere);
+    }
+}
